baa398edfb18eea9f9a35663818b622fb129cb62
nageshkumar13 <nageshkumar1925@gmail.com>
2024-06-18 14:22:52 +0530

    deleting files

--- ec2/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "myImage-ec2"{
-    ami = var.ami
-    instance_type = var.instance_type
-    user_data = file("serverfile.sh")
-    iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
-    tags = {
-    Name = "ec2-from-myImage"
-  }    
-}
--- ec2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "ami" {
-    default = "ami-034eddbd6d68e9bbd"
-}
-
-variable "instance_type" {
-    default = "t2.micro"
-}
-
